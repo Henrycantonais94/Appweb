59b9e4bd79635b2ee74d8c2b195de95b3555c7e7
Henrycantonais94 <henry-christian.majorel@eemi.com>
2024-09-26 14:23:50 +0200

    Update home.py

--- home.py
+++ home.py
@@ -10,4 +10,4 @@ user_input = st.text_input ("Tapez votre texte :")
 st.write (user_input)
 
 #Image
-st.image ("https://www.manga-city.fr/wp-content/uploads/2023/03/berserk-10.jpg")
+st.image ("https://www.livreshebdo.fr/sites/default/files/styles/article_principal/public/2022-06/Berserk.png?itok=hw6ZYicJ")
