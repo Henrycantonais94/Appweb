8ca69185f5e0a001446b3b374600d8452ef9d5eb
Henrycantonais94 <henry-christian.majorel@eemi.com>
2024-09-26 14:13:30 +0200

    Update home.py

--- home.py
+++ home.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.title ("Mon formulaire")
 
 #texte
-St.write ("Ceci est un formulaire de contact")
+st.write ("Ceci est un formulaire de contact")
 
 #Champ de saisi
-St.text_input ("Tapez votre texte : ")
+st.text_input ("Tapez votre texte : ")
